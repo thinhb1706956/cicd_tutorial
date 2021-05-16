9890809f86b7ce2076dbaa250f5a1e5aa047bfc8
phuc thinh <thinhb1706956@student.ctu.edu.vn>
2021-05-16 22:41:49 +0700

    second commit

--- flask_docker.py
+++ flask_docker.py
@@ -9,3 +9,11 @@ if __name__ == '_main_':
 
     app.run(debug=True,host='0.0.0.0')
 
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+
+def hello_world():
+    return 'Hello FOSS, Hello CI/CD using Jenkins'
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
